dd1a908d9b46be6e302f4a846013a7b70ad99310
naveenairani <naveenairani65@gmail.com>
2024-12-26 00:53:43 +0530

    removed similiar files

--- LeetCode/1431/1431.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-class Solution:
-    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        maxcandies = max(candies)
-        result = []
-        for candy in candies:
-            result.append(candy + extraCandies >= maxcandies)
-        return result
